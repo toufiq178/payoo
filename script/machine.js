b5af5b4785a3c21fc072f5faeb529ca3eefa4c5a
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-18 20:39:27 +0600

    show and hide feature added

--- script/machine.js
+++ script/machine.js
@@ -21,3 +21,20 @@ function setBalance(value) {
     balanceElement.innerText = value ;
 
 }
+
+
+// show  one
+function showOne(id) {
+    
+    // access all
+    const addMoney = document.getElementById('add-money-form');
+    const cashOut = document.getElementById('cash-out-form');
+
+    // hide all
+    addMoney.classList.add('hidden');
+    cashOut.classList.add('hidden');
+
+    // if click me show me   
+    const showOnly = document.getElementById(id);
+    showOnly.classList.remove('hidden')
+}
