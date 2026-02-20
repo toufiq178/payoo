545219b59655ab20bd02cedcb578bd3f21ff5f37
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-20 23:11:37 +0600

    recode starting

--- script/addMoney.js
+++ script/addMoney.js
@@ -1,78 +1,44 @@
+document.getElementById('add-money-btn').addEventListener('click', function(){
 
-document.getElementById("add-money-btn").addEventListener('click', function(){
-
-    //1 get the bank
-    const bankName = getInputById('add-money-bank');
-    
-    
-    if (bankName === 'Select bank') {
-        alert("please select a bank");
+    // 1 get the bank 
+    const addMoneyBank = getInputValueById('add-money-bank');
+    if (addMoneyBank === "Select bank") {
+        alert('please select an option');
         return;
     }
-    // console.log(bankName);
-    
 
-    // 2 get the bank acc number
-    const accNumber = getInputById('add-money-number');
-    if (accNumber.length !== 11) {
-        
-        alert('invalid number')
-    }
-
-    // 3 get amount to withdraw & calculate 
-    const addMoneyAmount = getInputById("add-money-amount");
-    const newBalance = getBalance() + Number(addMoneyAmount);
-    if (addMoneyAmount < 0 ) {
-        alert('invalid amount')
+    // 2 get bank number and check validation
+    const addMoneyNumber = getInputValueById('add-money-number');
+    if (addMoneyNumber.length !== 11) {
+        alert('invalid number');
         return;
     }
-    console.log(newBalance);
+
+    // 3 get amount 
+    const addMoneyAmount = getInputValueById('add-money-amount');
+    if (addMoneyAmount < 0) {
+        alert('invalid amount');
+        return;
+    }
     
+    // 4 calculate amount
+    const currentBalance = getBalance();
+    const newBalance = currentBalance + Number(addMoneyAmount) ;
+    
+    // 5 get the pin & check validate
+    const pin = getInputValueById('add-money-pin');
+    if (pin === '1234') {
 
-    // 4 get pin and check pin
-    const addMoneyPin = getInputById('add-money-pin');
-    if (addMoneyPin == '1234') {
-
-        // 4.1 true set the balance 
-        alert(` 
-            add money : ${addMoneyAmount} BDT.  successful
-            Total : ${newBalance}BDT.
-            ${new Date()}
-            
-            `);
+        // 5.1 true ::> alert success & set the balance
+        alert('successful add money');
         setBalance(newBalance);
 
 
-        // 1 get the history-container
-        const history = document.getElementById('history-container');
-        // 2 create a div inside history-container
-        const newDiv = document.createElement('div');
-
-        // 3 set the inner html
-        newDiv.innerHTML = `
-            <div class="border-neutral-200 border py-3 px-4 rounded-xl bg-neutral-50 shadow">
-
-                <div class="flex gap-3 items-center">
-                    <div class="flex justify-center items-center w-11 h-11 rounded-full bg-base-300">
-                        <img src="./payoo-resources/assets/opt-1.png" />
-                    </div>
-                    <div>
-                        <h1 class="label text-neutral/70 font-bold">Add Money</h1>
-                        <p class="text-xs uppercase font-normal opacity-60">${todayDate()} ${timeBd()}</p>
-                        <p class="text-xs uppercase font-normal opacity-80">Added Amount <span class = "font-bold text-[1rem]"> $${addMoneyAmount}</span></p>
-
-                    </div>
-                </div>
-            </div> 
-
-        ` 
-        // 4 append the div to history container  
-        history.appendChild(newDiv);
 
     }else{
-        // 4.2 false alert error
-        alert('invalid pin')
 
+        // 5.2 false ::> alert error return
+        alert('invalid pin');
+        return;
     }
-    
 })
--- script/machine.js
+++ script/machine.js
@@ -1,79 +1,50 @@
-// get input to value converter
-function getInputById(id) {
-
-    const input = document.getElementById(id);
-    const value = input.value;
+// get the input value 
+function getInputValueById(id) {
+    
+    const Input = document.getElementById(id);
+    const value = Input.value;
     return value;
 }
 
-// machine ::> get balance
+// get balance
 function getBalance() {
-
+    
     const balanceElement = document.getElementById('balance');
     const balance = balanceElement.innerText;
-    return Number(balance);
+    return Number(balance) ;
 }
 
-// machine ::> set the balance
-function setBalance(value) {
-
+// set balance
+function setBalance(newBalance) {
+    
     const balanceElement = document.getElementById('balance');
-    balanceElement.innerText = value;
-
+    balanceElement.innerText = newBalance;
+    
 }
 
 
-// show  one
+// show only 
 function showOne(id) {
-
-    // access all
-    const addMoney = document.getElementById('add-money-form');
-    const cashOut = document.getElementById('cash-out-form');
-    const history = document.getElementById('history');
-    const sendMoney = document.getElementById('transfer-money-form');
-    const payBiller = document.getElementById('pay-bill-form');
+    
+    // get the id
+    const addMoneyForm = document.getElementById('add-money-form');
+    const cashOutForm = document.getElementById('cash-out-form');
+    const transfer = document.getElementById('transfer-money-form');
     const getBonus = document.getElementById('get-bonus-form');
+    const payBill = document.getElementById('pay-bill-form');
+    const history = document.getElementById('history');
+
 
     // hide all
-    addMoney.classList.add('hidden');
-    cashOut.classList.add('hidden');
-    history.classList.add('hidden');
-    sendMoney.classList.add('hidden');
-    payBiller.classList.add('hidden');
+    addMoneyForm.classList.add('hidden');
+    cashOutForm.classList.add('hidden');
+    transfer.classList.add('hidden');
     getBonus.classList.add('hidden');
+    payBill.classList.add('hidden');
+    history.classList.add('hidden');
 
-    // if click me show me   
-    const showOnly = document.getElementById(id);
-    showOnly.classList.remove('hidden')
-}
 
-// date 
-function todayDate() {
-
-    const date = new Date();
-
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let currentDate = `${day}/${month}/${year}`;
-    return currentDate
-}
-
-// DATE 2
-function timeBd() {
-    const bdTime = new Date().toLocaleTimeString('en-US', {
-        timeZone: 'Asia/Dhaka',
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true
-    })
-    
-    return bdTime
-}
-
-// get parentage
-function getParentage(amount , parentage) {
-
-    const newAmount = amount + (amount * parentage / 100);
-    return newAmount;
+    // remove class
+    const removeClass = document.getElementById(id);
+    removeClass.classList.remove('hidden');
 }
